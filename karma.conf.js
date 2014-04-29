a52656a7777eaff49d6b654e2ee286c358f0b168
Graham Heath <Graham.Heath@rga.com>
2014-04-29 09:54:05 -0500

    karma config tweaks

--- karma.conf.js
+++ karma.conf.js
@@ -9,27 +9,32 @@ module.exports = function(config) {
     frameworks: ['jasmine'],
     
     plugins: [
-          'karma-jasmine',
-          'karma-coverage',
-          'karma-chrome-launcher',
-          'karma-phantomjs-launcher'
-      ],
+      'karma-jasmine',
+      'karma-coverage',
+      'karma-chrome-launcher',
+      'karma-phantomjs-launcher'
+    ],
 
     // list of files / patterns to load in the browser
     files: [
       'bower_components/jquery/jquery.js',
       'bower_components/angular/angular.js',
       'bower_components/angular-mocks/angular-mocks.js',
+      'bower_components/angular-ui/build/angular-ui.js',
       'bower_components/angular-resource/angular-resource.js',
       'bower_components/angular-route/angular-route.js',
       'bower_components/angular-touch/angular-touch.js',
       // 'bower_components/angular-scenario/angular-scenario.js',
       'bower_components/angular-animate/angular-animate.js',
+      'vendor/three.js/build/three.js',
+      'vendor/three.pointerlockcontrols.js',
+      'vendor/threex.domevent.js',
+      'vendor/threex.windowresize.js',
+      'vendor/perlin.js',
       'js/*.js',
       'js/**/*.js',
       {pattern:'css/**/*.css',watched:false,included:false},
-      {pattern:'fonts/**/*.otf',watched:false,included:false},//throws error, not testing for it either
-      {pattern:'views/**/*.html',included:false},
+      {pattern:'js/views/**/*.html',included:false},
       'test/mock/**/*.js',
       'test/spec/**/*.js'
     ],

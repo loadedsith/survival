ef3f34d680cfb29cef95a1d9fc63478128a010ed
Graham P Heath <graham.p.heath@gmail.com>
2014-05-23 22:22:13 -0500

    fixin links

--- js/services/ThreeJSConfig.js
+++ js/services/ThreeJSConfig.js
@@ -1,7 +1,7 @@
 angular.module('survivalApp').factory('ThreeJSConfigService', function ($resource) {
   'use strict';
   // This asset contains store:floors[]
-  return $resource('/threejs.json', {}, {
+  return $resource('threejs.json', {}, {
     retrieve : {method: 'GET', isArray: false}
   });
 });
